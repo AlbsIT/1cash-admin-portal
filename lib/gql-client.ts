de17642fb390d498c8de926ee4836a0ac434a192
Seifer Albacete <seiferalbacete@gmail.com>
2023-03-29 00:42:23 +0800

    del: removed old gql client

--- lib/gql-client.ts
+++ lib/gql-client.ts
@@ -1,6 +1,3 @@
-import {GraphQLClient} from 'graphql-request';
-
-export const gqlClient = new GraphQLClient(process.env.API_URL ?? 'https://1cash-api-93.azurewebsites.net/gql', {  });
 
 export const gqlRequest = async (query: string, variables?: any) => {
   const res = await fetch(process.env.API_URL ?? 'https://1cash-api-93.azurewebsites.net/gql', {
